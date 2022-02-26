c0c8fcd85bf6c06dcade6cb1d0f8c1ad45ac68b5
CameronCodingProjects <cameron2733@gmail.com>
2022-02-26 12:01:35 -0500

    Initial Commit

---
+++ CameronBusinessCard/CameronBusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameronBusinessCardApp.swift
+//  CameronBusinessCard
+//
+//  Created by Cameron Wright on 2/26/22.
+//
+
+import SwiftUI
+
+@main
+struct CameronBusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
